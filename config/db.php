8e924ac4119887d51a8b19f5eaa33bc6ec233434
Carlos Alberto Massavanhane Jr <carlos.massavanhane@jembi.org>
2021-05-04 11:00:22 +0200

    change database config

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii2course',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
